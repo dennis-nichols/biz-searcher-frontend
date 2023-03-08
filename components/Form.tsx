8e8bef6430806d9381bedb3e123725809510d976
Dennis Nichols <dgnichols22@gmail.com>
2023-03-08 12:34:25 -0500

    Feat: added table, got rid of csv download

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,83 +1,62 @@
 import React, { useState, FormEvent } from "react";
 import FormButton from "./FormButton";
+import { Table } from "./Table";
 
 interface Props {
   backendBaseUrl: string;
 }
 
 export default function Form({ backendBaseUrl }: Props) {
-  const [csvLink, setCsvLink] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
+  const [bizData, setBizData] = useState<[string, number][]>([]);
 
-  const downloadCSV = async (city: string, businessType: string) => {
+  const downloadData = async (
+    city: string,
+    businessType: string,
+    minRatings: number
+  ) => {
     try {
       const response = await fetch(
-        `${backendBaseUrl}?city=${city}&business_type=${businessType}`
+        `${backendBaseUrl}?city=${city}&business_type=${businessType}&min_ratings=${minRatings}`
       );
-
       if (response.ok) {
         let data = await response.json();
-
-        let csvContent = "data:text/csv;charset=utf-8,";
-
-        // extract the header names and join them with commas
-        const header = Object.keys(data).join(",");
-        // extract the column values and join them with line breaks
-        const values = (Object.values(data) as string[][])
-        console.log(values);
-        try {
-        let rowContent = "";
-        for (let i = 0; i < values.length; i++) {
-          let row = "";
-          for (let j = 0; j < values[i].length; j++) {
-            row += values[j][i] + ",";
-            if (j === values.length - 1) {
-              break
-            }
-          }
-          console.log(row)
-          row.slice(0, -1);
-          console.log(row)
-          rowContent += row + "\r\n";
-    
+        // extract the column values
+        const values = Object.values(data) as [string, number][];
+        const websites = values[1];
+        const user_ratings_total = values[0];
+        // create an array of objects with website and user_ratings_total properties
+        let bizData = [] as [string, number][];
+        for (let i = 0; i < websites.length; i++) {
+          bizData.push([websites[i] as string, user_ratings_total[i] as number]);
         }
-    
-        csvContent += header + "\r\n" + rowContent;
-      } catch (error) {
-        console.log(error)
-      }
-        
-
-        const encodedUri = encodeURI(csvContent);
-        const link = document.createElement("a");
-        link.setAttribute("href", encodedUri);
-        link.setAttribute("download", "search-results.csv");
-
-        setCsvLink(encodedUri);
+        setBizData(bizData);
         setError(null);
       } else {
         setError("An error occurred while processing your request.");
-        setCsvLink(null);
       }
     } catch (error) {
       setError("An error occurred while processing your request.");
-      setCsvLink(null);
     }
   };
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setLoading(true);
     const formData = new FormData(event.currentTarget);
     const city = formData.get("city") as string;
     const businessType = formData.get("businessType") as string;
+    const minRatings = parseInt(formData.get("minRatings") as string);
 
-    await downloadCSV(city, businessType);
-    setLoading(false);
+    setLoading(true);
+    await downloadData(city, businessType, minRatings).then(() => {
+      setLoading(false);
+    });
   };
 
+  console.log(bizData.length > 0 ? bizData : "No data yet");
   return (
+    <>
     <form
       className="flex flex-col items-center justify-center max-w-4xl mt-6 sm:w-full"
       onSubmit={handleSubmit}
@@ -95,20 +74,20 @@ export default function Form({ backendBaseUrl }: Props) {
         placeholder="Business Type"
         className="w-2/4 px-4 py-2 mt-4 text-black bg-gray-500 border-gray-700 rounded-md"
       />
-  
+
+      <input
+        type="number"
+        name="minRatings"
+        placeholder="Minimum Number of Ratings"
+        className="w-2/4 px-4 py-2 mt-4 text-black bg-gray-500 border-gray-700 rounded-md"
+      />
+
       <FormButton loading={loading} />
-  
-      {!loading && csvLink && (
-        <a
-          href={csvLink}
-          download="search-results.csv"
-          className="mt-4 text-green-500 hover:text-gray-400"
-        >
-          Download Results as CSV
-        </a>
-      )}
 
       {error && <p className="mt-4 text-red-600">{error}</p>}
+     
     </form>
+    {bizData.length > 0 && <Table bizData={bizData} />}
+    </>
   );
 }
---
+++ components/Table.tsx
@@ -0,0 +1,124 @@
+import React from "react";
+import { ReactNode } from "react";
+import {
+  ColumnDef,
+  useReactTable,
+  getCoreRowModel,
+  getSortedRowModel,
+  SortingState,
+} from "@tanstack/react-table";
+
+
+interface Props {
+  bizData: [string, number][];
+}
+
+type BizData = [string, number];
+
+export const Table: React.FC<Props> = ({ bizData }) => {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+
+  const columns = React.useMemo<ColumnDef<BizData>[]>(
+    () => [
+      {
+        header: "Website",
+        accessorFn: (row: BizData) => row[0],
+        cell: ({ getValue }) => getValue() as ReactNode,
+      },
+      {
+        header: "Number of Ratings",
+        accessorFn: (row: BizData) => row[1],
+        cell: ({ getValue }) => getValue() as ReactNode,
+      },
+    ],
+    []
+  );
+
+  const table = useReactTable({
+    data: bizData,
+    columns,
+    state: { sorting },
+    onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+  });
+
+  return (
+    <div className="m-2">
+      <table className="w-full p-2 mt-6 mb-6 border table-auto text-slate-400 border-slate-500">
+        <thead className="p-2 text-green-500 border-b border-slate-500">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr className="p-2" key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th
+                  className={
+                    header.column.columnDef.header === "Website"
+                      ? "pl-2 text-left"
+                      : "p-2"
+                  }
+                  key={header.id}
+                  colSpan={header.colSpan}
+                >
+                  {header.isPlaceholder ? null : (
+                    <div
+                      className={
+                        header.column.getCanSort()
+                          ? "cursor-pointer select-none"
+                          : ""
+                      }
+                      onClick={header.column.getToggleSortingHandler()}
+                    >
+                      {header.column.columnDef.header}
+                      {header.column.getIsSorted() === "asc"
+                        ? " 🔼"
+                        : header.column.getIsSorted() === "desc"
+                        ? " 🔽"
+                        : null}
+                    </div>
+                  )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr className="" key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td
+                  className={
+                    typeof cell.getValue() === "string"
+                      ? "p-2 text-left underline "
+                      : "p-2 text-center"
+                  }
+                  key={cell.id}
+                >
+                  {typeof cell.getValue() === "string" ? (
+                    <a
+                      className="visited:text-green-500"
+                      href="#"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        // Copy the cell content to the clipboard
+                        navigator.clipboard
+                          .writeText(cell.getValue())
+                          .then(() => {
+                            // Open the link in a new page
+                            window.open("https://" + cell.getValue(), "_blank");
+                          });
+                      }}
+                    >
+                      {cell.getValue()}
+                    </a>
+                  ) : (
+                    <span>{cell.getValue()}</span>
+                  )}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};

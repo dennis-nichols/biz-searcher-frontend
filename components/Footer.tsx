4aebc17ff7928ba64e67558bce418b4e8c9db12c
Dennis Nichols <dgnichols22@gmail.com>
2023-03-08 14:24:23 -0500

    added social icons to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   return (
     <footer className="flex items-center justify-center w-full h-24 text-gray-500 border-t border-gray-700">
-      <a
-        className="flex items-center justify-center gap-2"
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <p className="flex items-center justify-center gap-2">
         Built by Dennis Nichols, 2023
+      </p>
+      <a
+        target="_blank"
+        href="https://www.linkedin.com/in/dennisgnichols/"
+        className="w-5 ml-2"
+      >
+        {" "}
+        <FontAwesomeIcon icon={faLinkedin} size="3x" />
+      </a>
+      <a
+        target="_blank"
+        href="https://github.com/dennis-nichols"
+        className="w-5 ml-2"
+      >
+        {" "}
+        <FontAwesomeIcon icon={faGithub} size="3x" />
       </a>
     </footer>
   );

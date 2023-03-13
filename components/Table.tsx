244051373bb63ba8c2b0563dd99fea0f754bf786
Dennis Nichols <dgnichols22@gmail.com>
2023-03-13 12:40:27 -0400

    Feat: column data copy buttons

--- components/Table.tsx
+++ components/Table.tsx
@@ -69,13 +69,13 @@ export const Table: React.FC<Props> = ({ bizData }) => {
                       onClick={header.column.getToggleSortingHandler()}
                     >
                       <>
-                      {header.column.columnDef.header}
-                      {header.column.getIsSorted() === "asc"
-                        ? " 🔼"
-                        : header.column.getIsSorted() === "desc"
-                        ? " 🔽"
-                        : null}
-                        </>
+                        {header.column.columnDef.header}
+                        {header.column.getIsSorted() === "asc"
+                          ? " 🔼"
+                          : header.column.getIsSorted() === "desc"
+                          ? " 🔽"
+                          : null}
+                      </>
                     </div>
                   )}
                 </th>
@@ -84,43 +84,82 @@ export const Table: React.FC<Props> = ({ bizData }) => {
           ))}
         </thead>
         <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr className="" key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td
-                  className={
-                    typeof cell.getValue() === "string"
-                      ? "p-2 text-left md:text-base text-sm"
-                      : "p-2 text-center sm:w-1/3 md:text-base text-slate-400 text-sm"
-                  }
-                  key={cell.id}
-                >
-                  {typeof cell.getValue() === "string" ? (
-                    <a
-                      className="visited:text-green-500"
-                      href="#"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        // Copy the cell content to the clipboard
-                        navigator.clipboard
-                          .writeText(cell.getValue() as string)
-                          .then(() => {
-                            // Open the link in a new page
-                            window.open("https://" + cell.getValue(), "_blank");
-                          });
-                      }}
-                    >
-                      {cell.getValue() as string}
-                    </a>
-                  ) : (
-                    <span>{cell.getValue() as number}</span>
-                  )}
-                </td>
-              ))}
-            </tr>
+          {table.getRowModel().rows.map((row, index) => (
+            <>
+              <tr className="" key={row.id}>
+                {row.getVisibleCells().map((cell, cellIndex) => (
+                  <td
+                    className={
+                      typeof cell.getValue() === "string"
+                        ? "p-2 text-left md:text-base text-sm"
+                        : "p-2 text-center sm:w-1/3 md:text-base text-slate-400 text-sm"
+                    }
+                    key={cell.id}
+                  >
+                    {typeof cell.getValue() === "string" ? (
+                      <a
+                        className="visited:text-green-500"
+                        href="#"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          // Copy the cell content to the clipboard
+                          navigator.clipboard
+                            .writeText(cell.getValue() as string)
+                            .then(() => {
+                              // Open the link in a new page
+                              window.open(
+                                "https://" + cell.getValue(),
+                                "_blank"
+                              );
+                            });
+                        }}
+                      >
+                        {cell.getValue() as string}
+                      </a>
+                    ) : (
+                      <span>{cell.getValue() as number}</span>
+                    )}
+                  </td>
+                ))}
+              </tr>
+              {index === table.getRowModel().rows.length - 1 && (
+                <tr className="items-start p-4 text-left border-t border-slate-500">
+                  {table
+                    .getRowModel()
+                    .rows[0].getVisibleCells()
+                    .map((cell, cellIndex) => (
+                      <td key={cellIndex}>
+                        <button
+                          className={
+                            typeof cell.getValue() === "string"
+                              ? "hover:bg-green-500 hover:text-white text-green-500 font-mono rounded-lg p-1.5 text-sm m-2"
+                              : "hover:bg-green-500 hover:text-white text-green-500 font-mono rounded-lg p-1.5 ml-5 m-2  text-sm"
+                          }
+                          onClick={() => {
+                            // Get the values of the current column
+                            const columnValues = table
+                              .getRowModel()
+                              .rows.map((row) =>
+                                row.getVisibleCells()[cellIndex].getValue()
+                              )
+                              .join("\n");
+
+                            // Copy the column values to the clipboard
+                            navigator.clipboard.writeText(columnValues);
+                          }}
+                        >
+                          Copy Column
+                        </button>
+                      </td>
+                    ))}
+                </tr>
+              )}
+            </>
           ))}
         </tbody>
       </table>
     </div>
   );
 };
+
+

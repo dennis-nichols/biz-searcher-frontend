2a63669a147b733735574c129a9e3990d1640d4c
Dennis Nichols <dgnichols22@gmail.com>
2023-03-15 15:20:36 -0400

    Update page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-800">
       <Head>
-        <title>Ivan's Biz Searcher</title>
+        <title>Radman Co. Business Searcher</title>
         <link
           rel="icon"
           href="https://raw.githubusercontent.com/dennis-nichols/biz-searcher-frontend/main/public/favicon.ico"
